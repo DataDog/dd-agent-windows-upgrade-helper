bcbae9ed535b776310349b81f7baf4a1a34fd6e7
Derek Brown <derek.brown@datadoghq.com>
2017-03-05 18:59:33 -0800

    Temporary log to file

--- msidel.go
+++ msidel.go
@@ -16,6 +16,8 @@ const GENERIC_READ uint32 = 0x80000000
 const NO_ERROR syscall.Errno = 0
 const ERROR_SERVICE_DOES_NOT_EXIST syscall.Errno = 1060
 
+var ofile *os.File
+
 func isServiceInstalled() (installed bool, err error) {
 	var hSCM uintptr
 	var mod = syscall.NewLazyDLL("advapi32.dll")
@@ -27,7 +29,7 @@ func isServiceInstalled() (installed bool, err error) {
 	err = NO_ERROR
 	hSCM, _, err = openScManager.Call(uintptr(0), uintptr(0), uintptr(GENERIC_READ))
 	if hSCM == uintptr(0) {
-		fmt.Printf("Failed to open SC Manager %s\n", err)
+		fmt.Fprintf(ofile, "Failed to open SC Manager %s\n", err)
 		return
 	}
 	hService, _, err := openService.Call(hSCM,
@@ -38,7 +40,7 @@ func isServiceInstalled() (installed bool, err error) {
 		closeServiceHandle.Call(hService)
 		err = NO_ERROR
 	} else {
-		fmt.Printf("Failed to open Service %s\n", err)
+		fmt.Fprintf(ofile, "Failed to open Service %s\n", err)
 	}
 
 	if hSCM != uintptr(0) {
@@ -50,6 +52,16 @@ func isServiceInstalled() (installed bool, err error) {
 const ERROR_NO_MORE_DATA syscall.Errno = 259
 const cchGUID int = 38
 
+func convert_windows_string(winput []uint16) string {
+	var retstring string
+	for i := 0; i < len(winput); i++ {
+		if winput[i] == 0 {
+			break
+		}
+		retstring += string(rune(winput[i]))
+	}
+	return retstring
+}
 func findAndUninstallRelatedProducts(upgradeCode string, doUninstall bool) (bFound bool) {
 	bFound = false
 	var mod = syscall.NewLazyDLL("msi.dll")
@@ -72,6 +84,9 @@ func findAndUninstallRelatedProducts(upgradeCode string, doUninstall bool) (bFou
 				if uintptr(NO_ERROR) != uninstret {
 					return false
 				}
+			} else {
+				pc := convert_windows_string(buf)
+				fmt.Fprintf(ofile, "Found code: %s", pc)
 			}
 		}
 		index++
@@ -93,37 +108,37 @@ func mainfunc() int {
 	flag.Parse()
 
 	if oldCode == "" || newCode == "" {
-		fmt.Printf("Missing Parameter")
+		fmt.Fprintf(ofile, "Missing Parameter")
 		return -1
 	}
 
 	installed := findAndUninstallRelatedProducts(newCode, false)
 	if installed {
-		fmt.Printf("Found current product installed.")
+		fmt.Fprintf(ofile, "Found current product installed.")
 		return 0
 	}
 	installed = findAndUninstallRelatedProducts(oldCode, false)
 	if installed {
-		fmt.Printf("Found previous product as this user")
+		fmt.Fprintf(ofile, "Found previous product as this user")
 		if checkOnly {
-			fmt.Printf("Not uninstalling previous found version")
-			return 1638
+			fmt.Fprintf(ofile, "Not uninstalling previous found version")
+			return 1635
 		}
 		uninstalled := findAndUninstallRelatedProducts(oldCode, true)
 		if uninstalled {
-			fmt.Printf("Uninstalled previous product")
+			fmt.Fprintf(ofile, "Uninstalled previous product")
 			return 1
 		}
 		return -2
 	} else {
 		installed, err := isServiceInstalled()
-		fmt.Printf("isServiceInstalled: %t %s", installed, err)
+		fmt.Fprintf(ofile, "isServiceInstalled: %t %s", installed, err)
 		if installed {
-			fmt.Printf("service installed as other user")
-			return 1638
+			fmt.Fprintf(ofile, "service installed as other user")
+			return 1636
 		}
 		if !installed && (err == NO_ERROR || err == ERROR_SERVICE_DOES_NOT_EXIST) {
-			fmt.Printf("Service not installed")
+			fmt.Fprintf(ofile, "Service not installed")
 			return 0
 		}
 		return -2
@@ -131,5 +146,11 @@ func mainfunc() int {
 }
 
 func main() {
+	newfile, err := os.Create("c:\\tmp\\msidel.txt")
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+	ofile = newfile
+	defer ofile.Close()
 	os.Exit(mainfunc())
 }
